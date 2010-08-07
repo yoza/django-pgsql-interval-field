e085608a55e9898883e4efb2ad5f022e8ac03e0d
michal.dtz <michal.dtz@ca9aa62c-f11a-687e-08d0-aec0653d1e8e>
2010-08-07 22:39:32 +0000

    Bump release no
    git-svn-id: http://django-pgsql-interval-field.googlecode.com/svn/trunk@8 ca9aa62c-f11a-687e-08d0-aec0653d1e8e

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setupdict = dict(
     name = 'django-pgsql-interval-field',
-    version = '0.1',
+    version = '0.2',
     author = u'Michał Pasternak - FHU Kagami',
     author_email = 'michal.dtz@gmail.com',
     url = 'http://fhu-kagami.pl/',
