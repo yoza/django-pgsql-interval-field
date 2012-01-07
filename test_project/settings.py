5f7d13647fb43d67d5d858ce11f52a8994ef8320
michal.dtz@gmail.com <michal.dtz@gmail.com@ca9aa62c-f11a-687e-08d0-aec0653d1e8e>
2012-01-07 15:43:08 +0000

    Small check
    git-svn-id: http://django-pgsql-interval-field.googlecode.com/svn/trunk@23 ca9aa62c-f11a-687e-08d0-aec0653d1e8e

--- test_project/settings.py
+++ test_project/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 )
 
 ROOT_URLCONF = 'test_project.urls'
