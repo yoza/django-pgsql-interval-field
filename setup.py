b3125a2ccf0c217e11a8be78ee2b0316bfcde35c
michal.dtz@gmail.com <michal.dtz@gmail.com@ca9aa62c-f11a-687e-08d0-aec0653d1e8e>
2012-01-27 20:55:27 +0000

    Add a MANIFEST.in file
    git-svn-id: http://django-pgsql-interval-field.googlecode.com/svn/trunk@33 ca9aa62c-f11a-687e-08d0-aec0653d1e8e

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ from setuptools import find_packages
 
 setup(
     name='django-pgsql-interval-field',
-    version='0.9',
+    version='0.9.2',
     author=u'Michał Pasternak - FHU Kagami',
     author_email='michal.dtz@gmail.com',
     url='http://code.google.com/p/django-pgsql-interval-field/',
     license='MIT',
     description='Support for PostgreSQL INTERVAL for Django',
-    packages=find_packages(exclude='test_project'),
+    packages=find_packages(exclude=['test_project']),
     package_data={'interval': [
         'static/interval.css',
         'locale/pl/LC_MESSAGES/django.mo',
         'locale/pl/LC_MESSAGES/django.po',
     ]},
+    include_package_data=True,
     install_requires=['django'],
     zip_safe=False)
